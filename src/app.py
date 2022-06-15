569228f5797d51af5295b82daf9c5710484578b9
Galen-Dively <65838051+Galen-Dively@users.noreply.github.com>
2022-06-15 00:31:18 -0400

    Commenting code

--- src/app.py
+++ src/app.py
@@ -1,12 +1,20 @@
-from src.database import DatabaseManager
-from src.getCoins import getCoins
+from src.database import DatabaseManager # controls database
+from src.getCoins import getCoins # gets the coin info
+
+# TODO: refactor databaseManager, insertData is to complicated with not so good
+# code,
+# features: save databases, select databases, 
 
 
 class App:
     def __init__(self):
-        self.coins = getCoins()
+        self.coins = getCoins() # returns a list of dictionarys storing coins
         self.databaseManager = DatabaseManager()
+
         # write to database
+        # loop trough each dictionary in self.coins()
+        # creates a table in database,stored in dict
+        # {"Name: (str) name of table, "Vars"; a list of vars in table}
         for coin in self.coins:
             self.databaseManager.insertData({"Name": "cryptos", "Vars": ["DayChange", "Name", "Price"]}, [coin["Day Change"].replace("%", ""), coin["Name"], coin["Price"]])
 
